d5290d4ebc4e85ad15ccca589b57d2f6e8bd69cc
Jacob Sommer <jacob.sommer@comcast.net>
2017-09-09 21:49:56 -0700

    Clean up more code

--- mobile.js
+++ mobile.js
@@ -1,14 +1,14 @@
 var mobile = function() {
-	if(screen.width <= 800) {
-        document.getElementById('tabs').style.marginLeft = '0px';
-        document.getElementById('tabs').style.marginRight = '0px';
-		
-		if(document.getElementById('body1') != null) {
-            document.getElementById('body1').style.marginLeft = 'auto';
-            document.getElementById('body1').style.marginRight = 'auto';
+		if(screen.width <= 800) {
+		    document.getElementById('tabs').style.marginLeft = '0px';
+		    document.getElementById('tabs').style.marginRight = '0px';
+
+				if(document.getElementById('body1') != null) {
+		        document.getElementById('body1').style.marginLeft = 'auto';
+		        document.getElementById('body1').style.marginRight = 'auto';
+				}
+
+				document.p.style.fontSize = '1em';
+				document.a.style.fontSize = '1em';
 		}
-		
-		document.p.style.fontSize = '1em';
-		document.a.style.fontSize = '1em';
-	}
 }

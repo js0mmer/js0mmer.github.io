810a93006dbe9d1f2e71748a8be410e42579b91e
Jacob Sommer <jacob.sommer@comcast.net>
2018-10-27 04:32:05 +0000

    Mobile parallax support

--- js/parallax.js
+++ js/parallax.js
@@ -1,7 +1,17 @@
 /* global $ */
 
-$('.banner').css('background-position-y', -$(window).scrollTop() / 2);
+var mobile = screen.width <= 800;
+
+if(mobile) {
+    $('.banner').css('background-position-y', $(window).scrollTop() / 2);
+} else {
+    $('.banner').css('background-position-y', -$(window).scrollTop() / 2);
+}
 
 $(window).scroll(function() {
-  $('.banner').css('background-position-y', -$(window).scrollTop() / 2);
+    if(mobile) {
+        $('.banner').css('background-position-y', $(window).scrollTop() / 2);
+    } else {
+        $('.banner').css('background-position-y', -$(window).scrollTop() / 2);
+    }
 });

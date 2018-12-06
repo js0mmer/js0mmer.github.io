d50d79241afd75edf9963e8cd2f235e448a6bced
Jacob Sommer <jacob.sommer@comcast.net>
2018-12-06 00:54:16 +0000

    Added little banner movements

--- js/parallax.js
+++ js/parallax.js
@@ -1,17 +1,32 @@
 /* global $ */
 
 var mobile = screen.width <= 800;
+var offsetX = screen.width * 0.05;
+var offsetY = screen.height * 0.05;
 
 if(mobile) {
-    $('.banner').css('background-position-y', $(window).scrollTop() / 2);
+    $('.banner').css('background-position-y', $(window).scrollTop() / 2 - offsetY);
 } else {
-    $('.banner').css('background-position-y', -$(window).scrollTop() / 2);
+    $('.banner').css('background-position-y', -$(window).scrollTop() / 2 - offsetY);
 }
 
 $(window).scroll(function() {
     if(mobile) {
-        $('.banner').css('background-position-y', $(window).scrollTop() / 2);
+        $('.banner').css('background-position-y', $(window).scrollTop() / 2 - offsetY);
     } else {
-        $('.banner').css('background-position-y', -$(window).scrollTop() / 2);
+        $('.banner').css('background-position-y', -$(window).scrollTop() / 2 - offsetY);
     }
 });
+
+document.onmousemove = function(e) {
+	var x = (window.Event) ? e.pageX : event.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+	var y = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+	
+	$('.banner').css('background-position-x', (x - screen.width / 2) / -100 - offsetX);
+    
+	if(mobile) {
+	    $('.banner').css('background-position-y', $(window).scrollTop() / 2 + (y - screen.height / 2) / -100 - offsetY);
+	} else {
+    	$('.banner').css('background-position-y', -$(window).scrollTop() / 2 + (y - screen.height / 2) / -100 - offsetY);
+	}
+};

38c710069acec4dd0d89b6468a6dec613bbe88d1
Jacob Sommer <jacob.sommer@comcast.net>
2018-12-29 05:29:50 +0000

    Fix bg centering when resizing

--- js/parallax.js
+++ js/parallax.js
@@ -20,6 +20,7 @@ $(window).scroll(() => {
 // fix bg position when window resizes
 $(window).resize(() => {
   if (isMobile()) {
+    $('.banner').css('background-position-x', '50%');
     $('.banner').css('background-position-y', $(window).scrollTop() / 2);
   } else {
     $('.banner').css('background-position-y', -$(window).scrollTop() / 2);

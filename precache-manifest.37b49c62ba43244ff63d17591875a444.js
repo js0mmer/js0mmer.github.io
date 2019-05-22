4f1753883fd3b84caf941317c57affaf30e73717
Jacob Sommer <jacob.sommer@comcast.net>
2019-05-21 17:36:50 -0700

    Updates

---
+++ precache-manifest.37b49c62ba43244ff63d17591875a444.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df098a778a5bad0063f0498d63a62f11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b2401bfb5db5de750c7",
+    "url": "/static/css/main.66f4e57c.chunk.css"
+  },
+  {
+    "revision": "c232ed06d95e95318514",
+    "url": "/static/js/2.d1628c20.chunk.js"
+  },
+  {
+    "revision": "9b2401bfb5db5de750c7",
+    "url": "/static/js/main.009ffa28.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0bd50eeba34474772839de8da711da22",
+    "url": "/static/media/aerodynamic.0bd50eeb.jpg"
+  },
+  {
+    "revision": "3ace8af402c12c4d134114f8f512dea4",
+    "url": "/static/media/emile-perron-190221.3ace8af4.jpg"
+  },
+  {
+    "revision": "4568b5b5034960068b1cd0f69dab2039",
+    "url": "/static/media/fa-brands-400.4568b5b5.svg"
+  },
+  {
+    "revision": "cbd387d93e253048800dc9ee22b85c24",
+    "url": "/static/media/fa-brands-400.cbd387d9.eot"
+  },
+  {
+    "revision": "d30c44aaad8efa7626428c8294f7e880",
+    "url": "/static/media/fa-brands-400.d30c44aa.woff"
+  },
+  {
+    "revision": "d6ac6c968cff1abcbf5d548828b9f6c6",
+    "url": "/static/media/fa-brands-400.d6ac6c96.ttf"
+  },
+  {
+    "revision": "eac60e8a656781e13d2a674b4d9051c0",
+    "url": "/static/media/fa-brands-400.eac60e8a.woff2"
+  },
+  {
+    "revision": "26d8edc83280c58a277393d469e7b26f",
+    "url": "/static/media/fa-regular-400.26d8edc8.svg"
+  },
+  {
+    "revision": "4b218302f9057d02864d4909661831e9",
+    "url": "/static/media/fa-regular-400.4b218302.woff2"
+  },
+  {
+    "revision": "8c1f078070537f81237a2e39eaae2555",
+    "url": "/static/media/fa-regular-400.8c1f0780.eot"
+  },
+  {
+    "revision": "e32cb360659f3788d9f4b5750c2c5a36",
+    "url": "/static/media/fa-regular-400.e32cb360.woff"
+  },
+  {
+    "revision": "f1994ecd58b56afa035ae3da39213357",
+    "url": "/static/media/fa-regular-400.f1994ecd.ttf"
+  },
+  {
+    "revision": "2e302fa4c6eeb1bc06149067bae3e7b4",
+    "url": "/static/media/fa-solid-900.2e302fa4.eot"
+  },
+  {
+    "revision": "5dc01cfcd5336f696cb85da7ce53fa9b",
+    "url": "/static/media/fa-solid-900.5dc01cfc.woff2"
+  },
+  {
+    "revision": "80c404ff42e52d9e7589e83fe21307b4",
+    "url": "/static/media/fa-solid-900.80c404ff.ttf"
+  },
+  {
+    "revision": "a8eedaadb16b569a48a061d4aafa2d2e",
+    "url": "/static/media/fa-solid-900.a8eedaad.woff"
+  },
+  {
+    "revision": "b7c60297fd4238ed7bf0df6f8842c3df",
+    "url": "/static/media/fa-solid-900.b7c60297.svg"
+  },
+  {
+    "revision": "905b69e7528cfa4a409d4e242cdece34",
+    "url": "/static/media/joanna-kosinska-129039-unsplash.905b69e7.jpg"
+  },
+  {
+    "revision": "e8180045063aeaf4e5c4213af24183da",
+    "url": "/static/media/joanna-kosinska-478198.e8180045.jpg"
+  },
+  {
+    "revision": "3b2bafeca1242a80621e1415b7028f03",
+    "url": "/static/media/joshua-sortino-228788.3b2bafec.jpg"
+  },
+  {
+    "revision": "b43e9de5eed8266f05632046d7fd37a8",
+    "url": "/static/media/me.b43e9de5.jpg"
+  },
+  {
+    "revision": "d051358bf1490fb97480a316947946eb",
+    "url": "/static/media/project-compound.d051358b.jpg"
+  },
+  {
+    "revision": "d49a7a65aa7d8a2a9498182bc3a3d8ed",
+    "url": "/static/media/scratch.d49a7a65.jpg"
+  }
+]);

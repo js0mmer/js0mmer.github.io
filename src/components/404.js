f088f369f90b5707472f0e9d6de65ff95219b8ed
Jacob Sommer <jacob.sommer@comcast.net>
2020-01-26 11:39:33 -0800

    Clean up 404 page

--- src/components/404.js
+++ src/components/404.js
@@ -8,62 +8,15 @@ class Error404 extends Component {
   constructor(props) {
     super(props);
     this.transition = this.transition.bind(this);
-    this.handleScroll = this.handleScroll.bind(this);
-    this.state = { redirect: false, firstLaunch: true }
+    this.state = { redirect: false }
   }
 
   componentDidMount() {
     ReactGA.pageview(window.location.pathname + window.location.search);
-    document.title = 'Jacob Sommer';
+    document.title = 'Jacob Sommer | Page Not Found';
     document.querySelector('.transition').style.top = 'auto';
     document.querySelector('.transition').style.bottom = '-50px';
     document.querySelector('.transition').style.height = 0;
-    document.querySelector('body').classList.remove('menu-is-active');
-    window.addEventListener('scroll', this.handleScroll);
-
-    if (window.location.href.includes('#')) {
-      document.getElementById(window.location.href.split('#')[1]).scrollIntoView({ behavior: 'smooth', block: window.innerWidth <= 800 ? 'start' : 'center' });
-    } else window.scrollTo(0, 0);
-
-    if (sessionStorage.getItem('alreadyLaunched') != null) {
-      this.setState({ firstLaunch: false });
-    } else {
-      sessionStorage.setItem('alreadyLaunched', 'true');
-      this.setState({ firstLaunch: true });
-    }
-
-    // this.arrow = document.querySelector('.fa-chevron-down');
-    // this.arrow.style.opacity = document.documentElement.scrollTop > 0 ? 0 : 1;
-
-    if (this.state.firstLaunch) {
-      this.profilePic = document.querySelector('.profile-picture');
-      this.aboutH1 = document.querySelector('#about h1');
-      this.contactH1 = document.querySelector('#contact h1');
-      this.form = document.querySelector('form');
-    }
-
-    document.querySelector('.transition').style.top = 'auto';
-    document.querySelector('.transition').style.bottom = '-50px';
-    document.querySelector('.transition').style.height = 0;
-    document.querySelector('body').classList.remove('menu-is-active');
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-
-  handleScroll() {
-    var scrollTop = document.documentElement.scrollTop;
-    // this.arrow.style.opacity = scrollTop > 0 ? 0 : 1;
-
-    if (this.state.firstLaunch) {
-      var scrollBottom = scrollTop + window.innerHeight;
-
-      if (scrollBottom >= this.profilePic.offsetTop) this.profilePic.classList.add('animated', 'zoomIn');
-      if (scrollBottom >= this.aboutH1.offsetTop) this.aboutH1.classList.add('animated', 'fadeInDown');
-      if (scrollBottom >= this.contactH1.offsetTop) this.contactH1.classList.add('animated', 'fadeInDown');
-      if (scrollBottom >= this.form.offsetTop) this.form.classList.add('animated', 'fadeInLeft');
-    }
   }
 
   transition(to) {

de4b626b4507911cf3b955ed50d2a38aa3c734ff
Jacob Sommer <jacob.sommer@comcast.net>
2020-01-26 12:10:59 -0800

    Clean up

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ import Footer from './Footer';
 import me from '../images/me.jpg';
 import Particles from 'react-particles-js';
 import '../css/animate.min.css';
+import particlesConfig from '../particlesjs-config.json';
 
 function Typing(props) {
   if (props.firstLaunch) {
@@ -64,10 +65,10 @@ class Home extends Component {
       document.getElementById(window.location.href.split('#')[1]).scrollIntoView({ behavior: 'smooth', block: window.innerWidth <= 800 ? 'start' : 'center' });
     } else window.scrollTo(0, 0);
 
-    if (sessionStorage.getItem('alreadyLaunched')) {
+    if (sessionStorage.getItem('firstLaunch') != null) {
       this.setState({ firstLaunch: false });
     } else {
-      sessionStorage.setItem('alreadyLaunched', 'true');
+      sessionStorage.setItem('firstLaunch', 'true');
       this.setState({ firstLaunch: true });
     }
 
@@ -122,8 +123,7 @@ class Home extends Component {
               </div>
             </div>
             <div className="col-xl-6 particles">
-              <Particles height="50vh"
-                width="30vw" params={{"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#212529"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#212529","opacity":0.4,"width":1},"move":{"enable":true,"speed":3,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"repulse"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true}} />
+              <Particles height="50vh" width="30vw" params={particlesConfig} />
             </div>
           </div>
         </Parallax>

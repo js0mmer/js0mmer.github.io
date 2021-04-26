8a22b55c738042bde619b3cf56d5de153089565e
Jacob Sommer <jacob.sommer@comcast.net>
2021-04-26 09:57:43 -0700

    open mailto in new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,8 @@ function Footer() {
             <p>Made with <span className="fa fa-heart fa-lg"></span> by Jacob</p>
           </div>
           <div className="col-sm icons">
-            <a className="fa-stack fa-lg" href="mailto:jacob.sommer@comcast.net">
+            {/* eslint-disable-next-line */}
+            <a className="fa-stack fa-lg" href="mailto:jacob.sommer@comcast.net" target="_blank">
               <span className="fa fa-circle fa-stack-2x"></span>
               <span className="slide-up-effect">
                 <span className="fa fa-envelope fa-stack-1x"></span>

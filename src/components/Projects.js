5f16f83d796245c4139b79bc76121461f2de7ee8
Jacob Sommer <jacob.sommer@comcast.net>
2021-09-24 16:28:38 -0700

    Clean up projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -43,33 +43,6 @@ const PROJECTS = [
   },
 ]
 
-class ProjectPanel extends Component {
-  constructor(props) {
-    super(props);
-    this.content = React.createRef();
-  }
-
-  componentDidMount() {
-    this.content.current.innerHTML = this.props.src.content;
-  }
-
-  render() {
-    return (
-      <div className="row">
-        <div className="col-md-5">
-          <img className="red-border" src={'/images/' + this.props.id + '.' + this.props.src.imgType} alt={this.props.src.title} />
-        </div>
-        <div className="col-md-7">
-          <h3>{this.props.src.title}</h3>
-          {this.props.src.class ? <h5>{this.props.src.class}</h5> : null}
-          <h5>{this.props.src.date}</h5>
-          <p ref={this.content}></p>
-        </div>
-      </div>
-    );
-  }
-}
-
 class ProjectCard extends Component {
   constructor(props) {
     super(props);
@@ -82,13 +55,6 @@ class ProjectCard extends Component {
 
   render() {
     return (
-      // <a className="card i animated zoomIn" href={this.props.src.link ? this.props.src.link : 'projects/' + this.props.id} target={this.props.src.link ? 'blank' : null} onClick={this.onClick}>
-      //   <img className="card-img-top" src={'/images/' + this.props.id + '.' + this.props.src.imgType} alt={this.props.src.title} />
-      //   <div className="card-body">
-      //     <h5 className="card-title">{this.props.src.title}</h5>
-      //     <p className="card-text">{this.props.src.description}</p>
-      //   </div>
-      // </a>
       <div className="card project container">
         <div className="row">
           <div className="col-xl-6">
@@ -112,10 +78,7 @@ class Projects extends Component {
   constructor(props) {
     super(props);
     this.transition = this.transition.bind(this);
-    this.openProject = this.openProject.bind(this);
-    this.closeProject = this.closeProject.bind(this);
-    this.handleResize = this.handleResize.bind(this);
-    this.state = { redirect: false, col1: [], col2: [], projectId: false, projectSrc: false, scrollTop: 0, innerWidth: window.innerWidth }
+    this.state = { redirect: false }
   }
 
   componentDidMount() {
@@ -123,33 +86,6 @@ class Projects extends Component {
     document.title = 'Projects | Jacob Sommer';
     window.scrollTo(0, 0);
     document.querySelector('.transition').classList.add('out');
-
-    window.addEventListener('resize', this.handleResize);
-
-    this.setupCards();
-
-    // if url specifies a project i.e. /projects/hangman
-    var id = this.props.match.params.id;
-    if (id) {
-      var src = null;
-      for (var i = 0; i < PROJECTS.length; i++) {
-        if (PROJECTS[i].id === id) {
-          src = PROJECTS[i].src;
-          break;
-        }
-      }
-
-      if (!src || src.link) {
-        this.props.history.push('/projects');
-        return;
-      }
-
-      this.openProject(id, src);
-    }
-
-    setTimeout(() => {
-      document.querySelectorAll('.card').forEach(el => el.classList.remove('animated'));
-    }, 1000);
   }
 
   componentWillUnmount() {
@@ -158,74 +94,6 @@ class Projects extends Component {
 
   transition(to) {
     setTimeout(() => { this.setState({ redirect: to }) }, 300);
-  }
-
-  handleResize() {
-    var innerWidth = window.innerWidth;
-    if (innerWidth < 992 && this.state.innerWidth >= 992) {
-      this.setupCards();
-    } else if (innerWidth >= 992 && this.state.innerWidth < 992) {
-      this.setupCards();
-    }
-    this.setState({ innerWidth });
-  }
-
-  setupCards() {
-    var col1 = [];
-    var col2 = [];
-    for (var i = 0; i < PROJECTS.length; i++) {
-      col1.push(<ProjectCard key={i} id={PROJECTS[i].id} src={PROJECTS[i].src} open={this.openProject} />);
-    }
-    // if (window.innerWidth < 992) {
-    //   for (var i = 0; i < PROJECTS.length; i++) {
-    //     col1.push(<ProjectCard key={i} id={PROJECTS[i].id} src={PROJECTS[i].src} open={this.openProject} />);
-    //   }
-    // } else {
-    //   for (var j = 0; j < PROJECTS.length; j++) {
-    //     if (j % 2 === 0) {
-    //       col1.push(<ProjectCard key={j} id={PROJECTS[j].id} src={PROJECTS[j].src} open={this.openProject} />);
-    //     } else {
-    //       col2.push(<ProjectCard key={j} id={PROJECTS[j].id} src={PROJECTS[j].src} open={this.openProject} />);
-    //     }
-    //   }
-    // }
-
-    this.setState({ col1, col2 });
-  }
-
-  openProject(projectId, projectSrc) {
-    this.props.history.push('/projects/' + projectId);
-    ReactGA.pageview(window.location.pathname + window.location.search);
-    document.title = projectSrc.title + ' | Projects | Jacob Sommer';
-
-    var scrollTop = document.documentElement.scrollTop;
-    document.querySelector('.project-transition').classList.add('open');
-
-    setTimeout(() => {
-      this.setState({ projectId, projectSrc, scrollTop })
-      setTimeout(() => {
-        document.querySelector('h1').scrollIntoView();
-        document.querySelector('.project-transition').classList.remove('open');
-      }, 1);
-    }, 200);
-  }
-
-  closeProject(e) {
-    e.preventDefault();
-
-    this.props.history.push('/projects');
-    ReactGA.pageview(window.location.pathname + window.location.search);
-    document.title = 'Projects | Jacob Sommer';
-
-    document.querySelector('.project-transition').classList.add('open');
-
-    setTimeout(() => {
-      this.setState({ projectId: false, projectSrc: false });
-      setTimeout(() => {
-        window.scrollTo(0, this.state.scrollTop);
-        document.querySelector('.project-transition').classList.remove('open');
-      }, 1);
-    }, 200);
   }
 
   render() {
@@ -238,27 +106,8 @@ class Projects extends Component {
         <Header>Projects</Header>
         <section id="projects" className="container">
           <div className="cards" style={this.state.projectId ? { display: 'none'} : {}}>
-            {this.state.col1.map(item => item)}
+            {PROJECTS.map(project => <ProjectCard id={project.id} src={project.src} />)}
           </div>
-          {this.state.projectId ? 
-            <div>
-              <ProjectPanel id={this.state.projectId} src={this.state.projectSrc} />
-              <div className="flex-center">
-                {/* eslint-disable-next-line */}
-                <a className="animated-arrow" href="#" onClick={this.closeProject}>
-                  <span className="arrow">
-                    <span className="shaft"></span>
-                  </span>
-                  <span className="main">
-                    <span className="text">
-                      Back to Projects
-                    </span>
-                  </span>
-                </a>
-              </div>
-            </div>
-            : null
-          }
         </section>
         <Footer />
       </div>

9e15746ea708110c3e8cfaef95c72f669427ac11
Jacob Sommer <jacob.sommer@comcast.net>
2022-11-16 16:03:14 -0800

    Refactor projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,54 +2,28 @@ import React, { Component } from 'react';
 import Nav from '../components/Nav';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import hangman from './projects/hangman.json';
-import startracker from './projects/startracker.json';
-import playpic from './projects/playpic.json';
+import playpicImg from '../images/playpic.png';
+import hangmanImg from '../images/hangman.png';
+import startrackerImg from '../images/startracker.jpg';
 
-const PROJECTS = [
-  {
-    id: 'playpic',
-    src: playpic
-  },
-  {
-    id: 'hangman',
-    src: hangman
-  },
-  {
-    id: 'startracker',
-    src: startracker
-  },
-]
-
-class ProjectCard extends Component {
-  constructor(props) {
-    super(props);
-    this.content = React.createRef();
-  }
-
-  componentDidMount() {
-    this.content.current.innerHTML = this.props.src.content || '';
-  }
-
-  render() {
-    return (
-      <article className="card project container">
-        <div className="row">
-          <div className="col-xl-6">
-            <h2>{this.props.src.title}</h2>
-            <p>{this.props.src.date}</p>
-            <p ref={this.content} />
-            {this.props.src.links ? /* eslint-disable-next-line */
-              <p>{this.props.src.links.map(item => <a key={this.props.id} className={`${item.icon} fa-lg`} href={item.href} target="_blank" aria-label={item.label}></a>)}</p>
-            : null }
-          </div>
-          <div className="col-xl-6">
-            <img src={`/images/${this.props.id}.${this.props.src.imgType}`} alt={this.props.src.title} />
-          </div>
+function Project({ title, date, img, content, links }) {
+  return (
+    <article className="card project container">
+      <div className="row">
+        <div className="col-xl-6">
+          <h2>{title}</h2>
+          <p>{date}</p>
+          <p>{content}</p>
+          {links ? /* eslint-disable-next-line */
+            <p>{links.map(link => <a key={link.href} className={`${link.icon} fa-lg`} href={link.href} target="_blank" aria-label={link.label}></a>)}</p>
+          : null }
         </div>
-      </article>
-    );
-  }
+        <div className="col-xl-6">
+          <img src={img} alt={title} />
+        </div>
+      </div>
+    </article>
+  );
 }
 
 class Projects extends Component {
@@ -64,9 +38,45 @@ class Projects extends Component {
         <Nav active={1} />
         <Header>Projects</Header>
         <main id="projects" className="container">
-          <div className="cards">
-            {PROJECTS.map(project => <ProjectCard key={project.id} id={project.id} src={project.src} />)}
-          </div>
+          <Project
+            title="PlayPic"
+            date="February 2022"
+            img={playpicImg}
+            content="A website that creates a Spotify playlist that matches the mood of the image you upload."
+            links={[
+              {
+                "icon": "fas fa-external-link-alt",
+                "href": "https://devpost.com/software/hackuci-jwe3al",
+                "label": "devpost"
+              }
+            ]}
+          />
+          <Project
+            title="Hangman"
+            date="January 2021"
+            img={hangmanImg}
+            content="An online multiplayer game of hangman. Written using React + Socket.IO."
+            links={[
+              {
+                "icon": "fas fa-external-link-alt",
+                "href": "https://github.com/js0mmer/hangman",
+                "label": "github repository"
+              }
+            ]}
+          />
+          <Project
+            title="Star Tracker"
+            date="October - November 2019"
+            img={startrackerImg}
+            content="A barn door star tracker enabling you to take longer exposures of the night sky and capture deep sky objects with a DSLR. Runs on a Raspberry Pi connected to a WiFi network, controlled from the Blynk app."
+            links={[
+              {
+                "icon": "fas fa-external-link-alt",
+                "href": "https://github.com/js0mmer/startracker",
+                "label": "github repository"
+              }
+            ]}
+          />
         </main>
         <Footer />
       </>

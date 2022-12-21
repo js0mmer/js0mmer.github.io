9deb5c1723282fe28a9dadd9b3269269954a71de
Jacob Sommer <jacobgsommer@gmail.com>
2022-12-20 20:39:56 -0800

    Remove service worker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ReactGA from 'react-ga';
-import * as serviceWorker from './serviceWorker';
 import App from './App';
 import './css/bootstrap.min.css';
 import './css/main.css';
@@ -9,8 +8,6 @@ import './css/all.min.css';
 
 ReactGA.initialize('UA-133013455-2');
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />); 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+root.render(
+  <App />
+); 

7c7a7830109fe170851a330ca3643ce98e0744ea
Jacob Sommer <jacobgsommer@gmail.com>
2023-11-24 17:56:33 -0800

    Use npm for bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/main.css';
 import './css/dark-theme.scss';
 import './css/all.min.css';
